537e12a2cb994a3f1d43918f9f02f27e5bedc59c
Devraj <devrajbando@gmail.com>
2026-09-13 03:05:24 +0530

    API url added to server

--- server/src/config/passport.ts
+++ server/src/config/passport.ts
@@ -2,13 +2,15 @@ import passport from "passport";
 import { Strategy as GoogleStrategy } from "passport-google-oauth20";
 import { Strategy as GitHubStrategy, type Profile as GitHubProfile } from "passport-github2";
 import { prisma } from "../lib/prisma.js";
-
+const API_URL=process.env.API_URL
 passport.use(
   new GoogleStrategy(
     {
       clientID: process.env.GOOGLE_CLIENT_ID!,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
-      callbackURL: "/auth/google/callback",
+      callbackURL: process.env.NODE_ENV === 'production' 
+      ? `${API_URL}/auth/google/callback`
+      : '/auth/google/callback',
     },
     async (_accessToken, _refreshToken, profile, done) => {
       try {
